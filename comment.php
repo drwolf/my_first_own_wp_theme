f1f15336603bb839d24f49fc7b600bf6997e53be
drwolf <derrobertwolf@gmx.de>
2016-10-23 16:48:38 +0200

    a few more codes to the comment file

--- comment.php
+++ comment.php
@@ -1,3 +1,14 @@
-<?php
-   
-?>
+<div id="kommentar_formular">
+
+<h3 id="respond">Schreibe einen Kommentar:</h3>
+<form action="<?php echo get_option('siteurl'); ?>/wp-comments.php" method="post" id="commentform">
+        <p>
+            <input type="text" name="author" id="author" value="<?php echo $comment_author; ?>" size="22" />
+            <label for="author">Name</label>
+        </p>
+        
+        <p>
+            <input type="text" name="email" id="email" value="<?php echo $comment_author_email; ?>" size="22" tabindex="2" />
+            <label for="email">E-Mail<small>(wird nicht ver&ouml;ffentlicht)</small></label>
+        </p>
+</div>
